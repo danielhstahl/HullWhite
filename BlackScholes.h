eed2a53f36eee65573a38b58d0d5dddaa1ab1308
Daniel Stahl <danstahl1138@gmail.com>
2017-12-17 09:39:40 -0600

    made black scholes more general for autodiff

--- BlackScholes.h
+++ BlackScholes.h
@@ -3,22 +3,34 @@
 #include <cmath>
 
 template<typename S, typename Discount, typename K, typename Sigma>
-auto BSPut(const S &S0, const Discount &discount, const K &k, const Sigma &sigma){
+auto BSPutHelper(const S &S0, const Discount &discount, const K &k, const Sigma &sigma){
+    double s=sqrt(2.0);
+    auto d1=log(S0/(discount*k))/(sigma)+sigma*.5;
+    return S0*(.5*erf(d1/s)-.5)+k*discount*(.5-.5*(erf((d1-sigma)/s)));
+}
+
+template<typename S, typename Discount, typename K, typename Sigma>
+auto BSPut(const S &S0, const Discount &discount, const K &k, const Sigma &sigma)->decltype(BSPutHelper(S0, discount, k, sigma)){
     if(sigma>0){
-        double s=sqrt(2.0);
-        auto d1=log(S0/(discount*k))/(sigma)+sigma*.5;
-        return S0*(.5*erf(d1/s)-.5)+k*discount*(.5-.5*(erf((d1-sigma)/s)));
+        return BSPutHelper(S0, discount, k, sigma);
     }
     else{
         return k>S0?(k-S0)*discount:0.0;
     }
 }
+
 template<typename S, typename Discount, typename K, typename Sigma>
-auto BSCall(const S &S0, const Discount &discount, const K &k, const Sigma &sigma){ //note that sigma includes sqrt(t) term so in vanilla BS sigma is equal to volatility*sqrt(T)
+auto BSCallHelper(const S &S0, const Discount &discount, const K &k, const Sigma &sigma){ //note that sigma includes sqrt(t) term so in vanilla BS sigma is equal to volatility*sqrt(T)
+
+    double s=sqrt(2.0);
+    auto d1=log(S0/(discount*k))/(sigma)+sigma*.5;
+    return S0*(.5+.5*erf(d1/s))-k*discount*(.5+.5*(erf((d1-sigma)/s)));
+    
+}
+template<typename S, typename Discount, typename K, typename Sigma>
+auto BSCall(const S &S0, const Discount &discount, const K &k, const Sigma &sigma)->decltype(BSCallHelper(S0, discount, k, sigma)){ //note that sigma includes sqrt(t) term so in vanilla BS sigma is equal to volatility*sqrt(T)
     if(sigma>0){
-        double s=sqrt(2.0);
-        auto d1=log(S0/(discount*k))/(sigma)+sigma*.5;
-        return S0*(.5+.5*erf(d1/s))-k*discount*(.5+.5*(erf((d1-sigma)/s)));
+        return BSCallHelper(S0, discount, k, sigma);
     }
     else{
         return S0>k?(S0-k)*discount:0.0;
